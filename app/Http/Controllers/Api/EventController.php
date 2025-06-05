208dda1a21bac3b81d0d5f0d7c2ca32494daf8ce
christoper santos <christoper.patiho@gmail.com>
2025-06-04 23:15:46 -0600

    FEAT(backend) add visibility type API

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\EventVisibilityType;
 use App\Services\EventService;
 
 class EventController extends Controller
@@ -24,6 +25,23 @@ class EventController extends Controller
                     'events' => $events,
                 ],
                 'message' => 'Eventos recuperados con éxito',
+            ], 200);
+
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function visibilityTypes()
+    {
+        try {
+            $visibility_types = EventVisibilityType::all();
+
+            return response()->json([
+                'data' => [
+                    'visibility_types' => $visibility_types,
+                ],
+                'message' => 'Tipos de visibilidad recuperados con éxito',
             ], 200);
 
         } catch (\Exception $e) {
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(EventController::class)->group(function () {
     Route::get('/events', 'index')->name('api.events.index');
+    Route::get('/events-visibility-types', 'visibilityTypes')->name('api.events.visibility.types');
 });
 
 Route::controller(AuthController::class)->group(function () {
